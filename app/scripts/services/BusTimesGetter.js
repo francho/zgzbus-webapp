e3ec5ccd3279c5367b26d398272148d2b04678ab
Francho <lab@francho.org>
2013-10-24 01:30:11 +0200

    ¿works minified?

--- app/scripts/services/BusTimesGetter.js
+++ app/scripts/services/BusTimesGetter.js
@@ -4,7 +4,6 @@ angular.module('zgzbus').service('BusTimesGetter', function BusTimesGetter($http
     var currentQuery = null;
 
     function callWebService(busStopNumber) {
-        // var url = 'http://www.dndzgz.com/point';
 
         var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20' +
             'url%3D%22http%3A%2F%2Fwww.dndzgz.com%2Fpoint%3Fid%3D' + busStopNumber + '%26service%3Dbus%22%20' +
